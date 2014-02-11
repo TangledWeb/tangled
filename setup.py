834580cce025f97091fb4b2173f8d4f3b6059a0a
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-10 19:02:40 -0800

    Fix download_url in setup()
    Should point to /tags

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Tangled namespace and utilities',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
-    download_url='https://github.com/TangledWeb/tangled',
+    download_url='https://github.com/TangledWeb/tangled/tags',
     author='Wyatt Baldwin',
     author_email='self@wyattbaldwin.com',
     packages=[
