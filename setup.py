3e2dc5556f46e23448d2b2d814d37b478a2a8f54
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-03-22 11:06:29 -0700

    Back to development: 0.1a8.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled',
-    version='0.1a7',
+    version='0.1a8.dev0',
     description='Tangled namespace and utilities',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
