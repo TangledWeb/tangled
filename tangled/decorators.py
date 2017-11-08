b5b69a31e4e2e411051cae387912ecec82bc4441
Wyatt Baldwin <self@wyattbaldwin.com>
2017-11-08 10:09:50 -0800

    Allow cached properties specify their dependencies
    This allows a cached property to be reset automatically when any of its
    dependencies are set or deleted.
    
    There's one backward-incompatible change. Previously, deleting a
    property that had not been set (via access or directly) would raise an
    `AttributeError`, but now it won't. This is because it's always safe to
    access a cached property (unlike a normal instance attribute where if
    it's deleted, attempting to access it will fail).

--- tangled/decorators.py
+++ tangled/decorators.py
@@ -8,26 +8,120 @@ class cached_property:
 
     """Similar to @property but caches value on first access.
 
-    When the property is first accessed, its value is computed and set
-    as an instance attribute (i.e., in the instance's ``__dict__``).
-    Subsequent accesses will get the value directly from the instance's
-    ``__dict__`` (i.e., this descriptor will not be invoked).
+    When a cached property is first accessed, its value will be computed
+    and cached in the instance's ``__dict__``. Subsequent accesses will
+    retrieve the cached value from the instance's ``__dict__``.
 
     The property can be set and deleted as usual. When the property is
-    deleted, its value will be recomputed and re-set on the next access.
+    deleted, its value will be recomputed and reset on the next access.
+
+    It's safe to ``del`` a property that hasn't been set--this won't
+    raise an attribute error as might be expected since a cached
+    property can't really be deleted (since it will be recomputed the
+    next time it's accessed).
+
+    A cached property can specify its dependencies (other cached
+    properties) so that when its dependencies are set or deleted, the
+    cached property will be cleared and recomputed on next access::
+
+        >>> class T:
+        ...
+        ...     @cached_property
+        ...     def a(self):
+        ...         return 'a'
+        ...
+        ...     @cached_property('a')
+        ...     def b(self):
+        ...         return '%s + b' % self.a
+        ...
+        ...
+        >>> t = T()
+        >>> t.a
+        'a'
+        >>> t.b
+        'a + b'
+        >>> t.a = 'A'
+        >>> t.b
+        'A + b'
+
+    When a property has been set directly (as opposed to via access), it
+    won't be reset when its dependencies are set or deleted. If the
+    property is later cleared, it will then be recomputed::
+
+        >>> t = T()
+        >>> t.b = 'B'  # set t.b directly
+        >>> t.b
+        'B'
+        >>> t.a = 'A'
+        >>> t.b  # t.b was set directly, so setting t.a doesn't affect it
+        'B'
+        >>> del t.b
+        >>> t.b  # t.b was cleared, so it's computed from t.a
+        'A + b'
 
     """
 
-    def __init__(self, fget):
-        self.fget = fget
-        self.__name__ = fget.__name__
-        self.__doc__ = fget.__doc__
+    def __init__(self, *args):
+        if args and callable(args[0]):
+            self._set_fget(args[0])
+            dependencies = args[1:]
+        else:
+            dependencies = args
+        self.dependencies = set(dependencies)
+
+    def __call__(self, fget):
+        self._set_fget(fget)
+        return self
 
     def __get__(self, obj, cls=None):
         if obj is None:  # property accessed via class
             return self
-        obj.__dict__[self.__name__] = self.fget(obj)
+        if self.__name__ not in obj.__dict__:
+            obj.__dict__[self.__name__] = self.fget(obj)
+            obj.__dict__[self.set_directly_name] = False
         return obj.__dict__[self.__name__]
+
+    def __set__(self, obj, value):
+        obj.__dict__[self.__name__] = value
+        self._del_dependents(obj)
+        obj.__dict__[self.set_directly_name] = True
+
+    def __delete__(self, obj):
+        if self.__name__ in obj.__dict__:
+            del obj.__dict__[self.__name__]
+            self._del_dependents(obj)
+        obj.__dict__[self.set_directly_name] = False
+
+    def _set_fget(self, fget):
+        self.fget = fget
+        self.set_directly_name = self._set_directly_name(fget.__name__)
+        self.__name__ = fget.__name__
+        self.__doc__ = fget.__doc__
+
+    def _set_directly_name(self, name):
+        return '__%s_set_directly__' % name
+
+    def _del_dependents(self, obj):
+        # When this property is set or deleted, find its dependent
+        # cached properties and delete them so that their values will be
+        # recomputed on next access. Properties that were set directly
+        # will be skipped.
+        for name in dir(obj.__class__):
+            if name == self.__name__:
+                continue
+            attr = getattr(obj.__class__, name)
+            delete = (
+                # Is the attribute a cached property? If not, skip it.
+                isinstance(attr, self.__class__) and
+                # Is the updated property one of its dependencies?
+                self.__name__ in attr.dependencies and
+                # Is the attribute set on the instance?
+                name in obj.__dict__ and
+                # Was it set directly via `self.x = y`? If so, skip it.
+                not obj.__dict__.get(self._set_directly_name(name))
+            )
+            if delete:
+                delattr(obj, name)
 
 
 _ACTION_REGISTRY = {}
--- tangled/tests/test_decorators.py
+++ tangled/tests/test_decorators.py
@@ -1,6 +1,13 @@
 import unittest
+from doctest import DocTestSuite
 
+import tangled.decorators
 from tangled.decorators import cached_property
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(DocTestSuite(tangled.decorators))
+    return tests
 
 
 class Class:
@@ -8,6 +15,10 @@ class Class:
     @cached_property
     def cached(self):
         return 'cached'
+
+    @cached_property('cached')
+    def dependent(self):
+        return self.cached + '.xxx'
 
 
 class TestCachedProperty(unittest.TestCase):
@@ -29,8 +40,6 @@ class TestCachedProperty(unittest.TestCase):
 
     def test_del(self):
         obj = Class()
-        with self.assertRaises(AttributeError):
-            del obj.cached
         self.assertEqual(obj.cached, 'cached')
         del obj.cached
         self.assertEqual(obj.cached, 'cached')
@@ -39,5 +48,71 @@ class TestCachedProperty(unittest.TestCase):
         del obj.cached
         self.assertEqual(obj.cached, 'cached')
         del obj.cached
-        with self.assertRaises(AttributeError):
-            del obj.cached
+
+
+class ClassWithDependentProperties(Class):
+
+    @cached_property('cached')
+    def dependent(self):
+        return self.cached + '.xxx'
+
+
+class TestCachedPropertyWithDependencies(unittest.TestCase):
+
+    def test_get(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+
+    def test_set(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        obj.dependent = 'XXX'
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'XXX')
+
+    def test_del(self):
+        obj = ClassWithDependentProperties()
+        del obj.dependent
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        del obj.dependent
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+
+    def test_set_dependency(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        obj.cached = 'new'
+        self.assertEqual(obj.cached, 'new')
+        self.assertEqual(obj.dependent, 'new.xxx')
+
+    def test_del_dependency(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        del obj.cached
+        self.assertEqual(obj.cached, 'cached')
+        self.assertEqual(obj.dependent, 'cached.xxx')
+
+    def test_get_set(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        obj.dependent = 'XXX'
+        self.assertEqual(obj.dependent, 'XXX')
+
+    def test_set_directly_then_del_dependency(self):
+        obj = ClassWithDependentProperties()
+        self.assertEqual(obj.dependent, 'cached.xxx')
+        obj.dependent = 'XXX'
+        self.assertEqual(obj.dependent, 'XXX')
+        del obj.cached
+        # obj.dependent was set directly, so it's not reset when
+        # obj.cached is deleted.
+        self.assertEqual(obj.dependent, 'XXX')
+        # Resetting obj.dependent will cause it to be recomputed.
+        del obj.dependent
+        obj.cached = 'new'
+        self.assertEqual(obj.dependent, 'new.xxx')
