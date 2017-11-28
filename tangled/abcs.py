5cafa4f464b91cd30ee4f09d325df6e234d515e9
Wyatt Baldwin <self@wyattbaldwin.com>
2017-11-27 17:59:29 -0800

    Remove ACommand.exit()

--- tangled/abcs.py
+++ tangled/abcs.py
@@ -22,16 +22,3 @@ class ACommand(metaclass=ABCMeta):
 
     def print_error(self, *args, file=sys.stderr, **kwargs):
         print(*args, file=file, **kwargs)
-
-    def exit(self, message=None, status=1):
-        """Exit with error code by default.
-
-        If ``message`` is passed, it will be printed to stderr.
-
-        """
-        if message:
-            if status == 0:
-                print(message)
-            else:
-                self.print_error(message)
-        sys.exit(status)
